fc9e7fadeb57f68dc3753d9c6b52e086accd7b45
Asif Mammadov <am14089@nyu.edu>
2024-11-19 14:48:50 -0500

    added recursion for big directories

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 
@@ -20,7 +22,39 @@ const (
 	defaultCephClusterLabel = "ceph"
 	defaultCephConfigPath   = "/etc/ceph/ceph.conf"
 	defaultCephUser         = "admin"
+	maxStackSize            = 10000
+	directorySizeToRecursse = 100_000_000_000 // 100 GB
 )
+
+type StackEntry struct {
+	items []PathEntry
+}
+
+func (s *StackEntry) Push(data PathEntry) error {
+	if s.Len() > maxStackSize {
+		errMsg := fmt.Sprintf("Stack overflow. Stack is bigger than defined maxStackSize: %d", maxStackSize)
+		return errors.New(errMsg)
+	}
+	s.items = append(s.items, data)
+	return nil
+}
+
+func (s *StackEntry) Pop() (*PathEntry, error) {
+	if s.IsEmpty() {
+		return nil, fmt.Errorf("stack is empty")
+	}
+	topEntry := s.items[len(s.items)-1]
+	s.items = s.items[:len(s.items)-1]
+	return &topEntry, nil
+}
+
+func (s *StackEntry) Len() int {
+	return len(s.items)
+}
+
+func (s *StackEntry) IsEmpty() bool {
+	return len(s.items) == 0
+}
 
 type PathEntry struct {
 	Organisation string `json:"Organisation"`
@@ -59,7 +93,7 @@ func (dm *DynamicMetrics) AddGauge(name, help string) prometheus.GaugeVec {
 	return *gauge
 }
 
-func LoadDirsConfig(path string) []PathEntry {
+func LoadDirsConfig(path string) StackEntry {
 	data, err := os.ReadFile(path)
 	if err != nil {
 		log.Fatalf("Failed to read JSON file: %v", err)
@@ -69,16 +103,30 @@ func LoadDirsConfig(path string) []PathEntry {
 	if err := json.Unmarshal(data, &entries); err != nil {
 		log.Fatalf("Failed to unmarshal JSON: %v", err)
 	}
-	return entries
+	se := StackEntry{}
+	for _, pathEntry := range entries {
+		err := se.Push(pathEntry)
+		if err != nil {
+			log.Fatal("Unable to read the config file. Error: ", err)
+		}
+	}
+
+	return se
 }
 
 func (dm *DynamicMetrics) UpdateMetrics(info *cephfs.MountInfo) {
 	attrs := []string{"ceph.dir.rbytes", "ceph.dir.rentries", "ceph.dir.rfiles"}
 	entries := LoadDirsConfig("paths.json")
-	for _, entry := range entries {
-		fmt.Printf("Organisation: %s, User: %s, Path: %s\n", entry.Organisation, entry.User, entry.Path)
-	}
-	for _, entry := range entries {
+
+	for {
+		if entries.IsEmpty() {
+			break
+		}
+		entry, err := entries.Pop()
+		if err != nil {
+			fmt.Println("Unable to get the next entry from the stack. Error: ", err)
+			break
+		}
 		for _, attr := range attrs {
 			value, err := info.GetXattr(entry.Path, attr)
 			if err != nil {
@@ -94,6 +142,37 @@ func (dm *DynamicMetrics) UpdateMetrics(info *cephfs.MountInfo) {
 			name := fmt.Sprintf("cephfs_xattr_%s", strings.ReplaceAll(attr, ".", "_"))
 			gauge := dm.AddGauge(name, "A dynamically generated metric")
 			gauge.With(entry.Tags()).Set(f)
+			if attr == "ceph.dir.rbytes" {
+				if f > directorySizeToRecursse {
+					dir, err := info.OpenDir(entry.Path)
+					if err != nil {
+						fmt.Println("Unable to open directory:", entry.Path)
+					}
+					for {
+						entryDir, err := dir.ReadDir()
+						if err != nil {
+							fmt.Println("Unable to read directory:", entry.Path)
+						}
+						if entryDir == nil {
+							break
+						}
+						if entryDir.Name() == "." || entryDir.Name() == ".." {
+							continue
+						}
+						if entryDir.DType() == 4 {
+							newEntry := PathEntry{
+								Organisation: entry.Organisation,
+								User:         entry.User,
+								Path:         filepath.Join(entry.Path, entryDir.Name()),
+							}
+							err := entries.Push(newEntry)
+							if err != nil {
+								fmt.Printf("Unable to add new entry. Error: %s", err)
+							}
+						}
+					}
+				}
+			}
 		}
 	}
 }
